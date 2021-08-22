37b16ac8a2f7a5d12d114509a13167af251b7455
bplok20010 <nobo.zhou@foxmail.com>
2021-08-22 22:21:58 +0800

    chore: 测试用例更新

--- tests/adsorbCreator.test.ts
+++ tests/adsorbCreator.test.ts
@@ -1,7 +1,7 @@
 import { createRefLine } from "../src";
 
 describe("test adsorbCreator", () => {
-  test("basic", () => {
+  test("basic 01", () => {
     const rect1 = {
       key: "a",
       left: 0,
@@ -35,7 +35,15 @@ describe("test adsorbCreator", () => {
         pageY: 3,
       })
     ).toMatchObject({
+      raw: {
+        left: 3,
+        top: 3,
+      },
       delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
         left: 0,
         top: 0,
       },
@@ -51,7 +59,15 @@ describe("test adsorbCreator", () => {
         pageY: 5,
       })
     ).toMatchObject({
+      raw: {
+        left: 5,
+        top: 5,
+      },
       delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
         left: 0,
         top: 0,
       },
@@ -67,7 +83,15 @@ describe("test adsorbCreator", () => {
         pageY: 6,
       })
     ).toMatchObject({
+      raw: {
+        left: 6,
+        top: 6,
+      },
       delta: {
+        left: 6,
+        top: 6,
+      },
+      offset: {
         left: 6,
         top: 6,
       },
@@ -188,6 +212,334 @@ describe("test adsorbCreator", () => {
         top: 8,
       },
     });
+  });
+
+  test("basic 02", () => {
+    const rect1 = {
+      key: "a",
+      left: 0,
+      top: 0,
+      width: 100,
+      height: 100,
+    };
+
+    const rect2 = {
+      key: "b",
+      left: 0,
+      top: 0,
+      width: 100,
+      height: 100,
+    };
+
+    const refLine = createRefLine({
+      rects: [rect1],
+    });
+
+    const updater = refLine.adsorbCreator({
+      current: rect2,
+      pageX: 0,
+      pageY: 0,
+      distance: 5,
+    });
+
+    let ret;
+
+    expect(
+      (ret = updater({
+        pageX: 3,
+        pageY: 3,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: 3,
+        top: 3,
+      },
+      delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
+        left: 0,
+        top: 0,
+      },
+      rect: {
+        left: 0,
+        top: 0,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: 5,
+        pageY: 5,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: 5,
+        top: 5,
+      },
+      delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
+        left: 0,
+        top: 0,
+      },
+      rect: {
+        left: 0,
+        top: 0,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: 6,
+        pageY: 6,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: 6,
+        top: 6,
+      },
+      delta: {
+        left: 6,
+        top: 6,
+      },
+      offset: {
+        left: 6,
+        top: 6,
+      },
+      rect: {
+        left: 6,
+        top: 6,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: 8,
+        pageY: 8,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: 2,
+        top: 2,
+      },
+      delta: {
+        left: 2,
+        top: 2,
+      },
+      offset: {
+        left: 8,
+        top: 8,
+      },
+      rect: {
+        left: 8,
+        top: 8,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: 5,
+        pageY: 5,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: -3,
+        top: -3,
+      },
+      delta: {
+        left: -8,
+        top: -8,
+      },
+      offset: {
+        left: 0,
+        top: 0,
+      },
+      rect: {
+        left: 0,
+        top: 0,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: 1,
+        pageY: 1,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: 1,
+        top: 1,
+      },
+      delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
+        left: 0,
+        top: 0,
+      },
+      rect: {
+        left: 0,
+        top: 0,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: -1,
+        pageY: -1,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: -1,
+        top: -1,
+      },
+      delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
+        left: 0,
+        top: 0,
+      },
+      rect: {
+        left: 0,
+        top: 0,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: -5,
+        pageY: -5,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: -5,
+        top: -5,
+      },
+      delta: {
+        left: 0,
+        top: 0,
+      },
+      offset: {
+        left: 0,
+        top: 0,
+      },
+      rect: {
+        left: 0,
+        top: 0,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: -6,
+        pageY: -6,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: -6,
+        top: -6,
+      },
+      delta: {
+        left: -6,
+        top: -6,
+      },
+      offset: {
+        left: -6,
+        top: -6,
+      },
+      rect: {
+        left: -6,
+        top: -6,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: -8,
+        pageY: -8,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: -2,
+        top: -2,
+      },
+      delta: {
+        left: -2,
+        top: -2,
+      },
+      offset: {
+        left: -8,
+        top: -8,
+      },
+      rect: {
+        left: -8,
+        top: -8,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
+
+    expect(
+      (ret = updater({
+        pageX: 8,
+        pageY: 8,
+      }))
+    ).toMatchObject({
+      raw: {
+        left: 16,
+        top: 16,
+      },
+      delta: {
+        left: 16,
+        top: 16,
+      },
+      offset: {
+        left: 8,
+        top: 8,
+      },
+      rect: {
+        left: 8,
+        top: 8,
+      },
+    });
+
+    rect2.left += ret.delta.left;
+    rect2.top += ret.delta.top;
   });
 
   test("basic scale 0.5", () => {
